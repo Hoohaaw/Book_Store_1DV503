11c1d26e0398bc60a07bfeb04a655d65c032d118
Fredrik Gischke <fredrik.gischke@gmail.com>
2026-01-08 12:16:16 +0100

    book search

---
+++ src/routes/books.js
@@ -0,0 +1,35 @@
+//Search books
+Router.get('/search', async (req, res) => {
+    const { author, title, subject, page = 1, limit = 5} = req.query;
+    const offset = (page - 1) * limit;
+    let query = `SELECT * FROM books WHERE 1=1`;
+    let params = [];
+    let idx = 1;
+
+    if (author) {
+        query += ` AND LOWER(author_first_name) LIKE LOWER($${idx} || '%')`;
+        params.push(author);
+        idx++;
+    }
+
+    if (title) {
+        query += ` AND LOWER(title) LIKE '%' || LOWER($${idx}) || '%'`;
+        params.push(title);
+        idx++;
+    }
+
+    if (subject) {
+        query += ` AND LOWER(subject) = LOWER($${idx})`;
+        params.push(subject);
+        idx++;
+    }
+    query += `ORDER BY title LIMIT $${idx} OFFSET $${idx +1}`;
+    params.push(limit, offset);
+
+    const { rows } = await db.query(query, params);
+    if (rows.length === 0 ) {
+        return res.json({ message: 'Inga böcker hittades'});
+    }
+    res.json(rows);
+
+})
